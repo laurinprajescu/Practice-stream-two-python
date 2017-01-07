854df8b1537c2dac5e056ab329f469e9b1d51f56
laurinprajescu <laurinprajescu@gmail.com>
2017-01-07 16:15:40 +0000

    challange guess_the _number - number of guesses left / test vending machine - first test

--- guess_the_number.py
+++ guess_the_number.py
@@ -10,12 +10,20 @@ while True:
     # give the user a certain amount of guesses
     for i in range(NUMBER_OF_GUESSES_FROM_USER):
         guess = int(raw_input('guess the number: '))
+
+        # counts the number of guesses left
+        guesses_left = NUMBER_OF_GUESSES_FROM_USER - i
+
         if guess == random_number:
             print 'well done'
             break
         elif guess > random_number:
             print "too high"
+            print "Guesses left: " + str(guesses_left)
         else:
             print "too low"
+            print "Guesses left: " + str(guesses_left)
 
-    print "let's try a new number!"
+    print "let's try a new number ! "
+
+    print "Your number should be in range: " + str(RANGE_TOP)
---
+++ test_vending_machine.py
@@ -0,0 +1,9 @@
+import unittest
+from vending_machine import give_change
+
+
+
+class TestVendingMachine(unittest.TestCase):
+    def test_return_change(self):
+        coins = give_change(17)
+        self.assertEqual(coins, [10, 5, 2], 'wrong change given')
