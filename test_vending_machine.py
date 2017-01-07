754c78aaa336a8fe7f9645a6b44cfa56262ea4ed
laurinprajescu <laurinprajescu@gmail.com>
2017-01-07 16:30:13 +0000

    vending-machine - second test

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -5,5 +5,5 @@ from vending_machine import give_change
 
 class TestVendingMachine(unittest.TestCase):
     def test_return_change(self):
-        coins = give_change(17)
-        self.assertEqual(coins, [10, 5, 2], 'wrong change given')
+        self.assertEqual(give_change(17), [10, 5, 2], 'wrong change given')
+        self.assertEqual(give_change(18), [10, 5, 2, 1], 'wrong change given')
