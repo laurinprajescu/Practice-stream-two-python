26f1bed7c39777a11867dca9951d2051e43dc99d
laurinprajescu <laurinprajescu@gmail.com>
2017-02-05 17:16:45 +0000

    lexical diversity

--- laurin-tweeter/tweet_access.py
+++ laurin-tweeter/tweet_access.py
@@ -15,6 +15,22 @@ api = tweepy.API(auth)
 count = 10
 query = 'Dublin'
 
+results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
+
+status_texts = [ status._json['text'] for status in results ]
+
+screen_names = [ status._json['user']['screen_name']
+                                for status in results
+                                        for mention in status._json['entities']['user_mentions'] ]
+
+hashtags = [ hashtag['text']
+                                for status in results
+                                        for hashtag in status._json['entities']['hashtags'] ]
+
+words = [ word
+                        for text in status_texts
+                                for word in text.split()]
+
 # get status
 results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
 print json.dumps(results[0]._json, indent=4)
@@ -26,3 +42,15 @@ for status in results:
     print status.user.profile_image_url_https
     print status.user.followers_count
     print status.place
+
+def get_lexical_diversity(items):
+    return 1.0*len(set(items))/len(items)
+
+def get_average_words(tweets):
+    total_words = sum([len(tweet.split()) for tweet in tweets ])
+    return 1.0*total_words/len(tweets)
+
+print "Average words: %s" % get_average_words(status_texts)
+print "Word Diversity: %s" % get_lexical_diversity(words)
+print "Screen Name Diversity: %s" % get_lexical_diversity(screen_names)
+print "Hashtag Diversity: %s" % get_lexical_diversity(hashtags)
