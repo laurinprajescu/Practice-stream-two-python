31d0a1f8e107a073ec4b8622afbfc799644ac8f5
laurinprajescu <laurinprajescu@gmail.com>
2017-02-05 18:03:00 +0000

    tweet popularity

---
+++ laurin-tweeter/tweeter_user.py
@@ -0,0 +1,21 @@
+import tweepy
+from tweepy import OAuthHandler
+
+CONSUMER_KEY = 'Ll01SzXcptfheD9hTiwZVv0go'
+CONSUMER_SECRET = 'y6lN2sfhMxYeELEoIU1CTfLlOvlAKFHUuy7ro9z0swmYanl1cK'
+OAUTH_TOKEN = '826864721563439104-ShcuZHoWOy352xa2UbZA1yszQk2CYQt'
+OAUTH_TOKEN_SECRET = '8pgxNum1pOvUF0g25WLG49SzlfqBqT78kCgxzdrHRwaY5'
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+user = api.get_user('@madonna')
+
+print user.screen_name
+print user.followers_count
+
+for friend in user.friends():
+    print
+    print friend.screen_name
+    print friend.followers_count
